1eda58932829337f0801c3b221d8bde13db4ef1b
ClmtHua <45039677+ClmtHua@users.noreply.github.com>
2021-05-05 14:49:21 +0200

    Update GetData_GSE138002.sh

--- Source/GetData_GSE138002.sh
+++ Source/GetData_GSE138002.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Download files from GSE138002
+mkdir data
 cd data
 wget -c "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE138002&format=file&file=GSE138002%5FFinal%5Fmatrix%2Emtx%2Egz" --output-document  GSE138002_Final_matrix.mtx.gz  &
 wget -c "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE138002&format=file&file=GSE138002%5FFinal%5Fbarcodes%2Ecsv%2Egz" --output-document GSE138002_Final_barcodes.csv.gz &
